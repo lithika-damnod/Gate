353e331d410cf008d0705c70d522f5476ad66cf1
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2024-11-14 16:09:51 +0530

    reinitiated the angular project since --no-standalone flag had not been used when creating the project

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ComponentsModule } from './components/components.module';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet,
-    ComponentsModule
-  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
