75f7d546fb3ccfbb5f3e06991c1e74f67174d717
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2024-11-14 18:30:16 +0530

    base

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavigationComponent } from "../core/";
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavigationComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [NavigationComponent],
 })
 export class CoreModule { }
