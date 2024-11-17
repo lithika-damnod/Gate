7af97b91c1c8597cfaad8be216682e658dcfeaba
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2024-11-17 14:37:11 +0530

    added links in the navbar

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from "../core/";
+import { SharedModule } from "../shared/shared.module";
 
 
 
 @NgModule({
   declarations: [NavigationComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [NavigationComponent],
 })
