fe32af3bdaa49140073dcf0e12351b987c85aff0
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2024-11-25 00:59:19 +0530

    refactor: set up routing functionality in angular application (#11)

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { EventComponent } from './pages/event/event.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'event', component: EventComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
