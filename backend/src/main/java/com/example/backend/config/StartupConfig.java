aca6b9dacd3a24f443d10952d81c1708e853b1df
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2024-12-09 23:08:05 +0530

    removed unused shell command class UserCommands which I created for testing purposes

--- backend/src/main/java/com/example/backend/config/StartupConfig.java
+++ backend/src/main/java/com/example/backend/config/StartupConfig.java
@@ -1,7 +1,6 @@
 package com.example.backend.config;
 
 import com.example.backend.service.StartupService;
-import com.example.backend.shell.commands.UserCommands;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
 public class StartupConfig {
 
     private final StartupService startupService;
-    private final UserCommands userCommands;
 
     @PostConstruct
     public void init() {
