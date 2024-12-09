940d16a236ac4da807e4f756b739098f4fa6c449
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2024-12-09 20:49:01 +0530

    fixed the bug: removed hero section because of deadline requirements and added state options to event-cards

--- frontend/src/app/shared/components/event-card/event-card.component.ts
+++ frontend/src/app/shared/components/event-card/event-card.component.ts
@@ -1,10 +1,14 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'app-event-card',
-    templateUrl: './event-card.component.html',
-    standalone: false
+  selector: 'app-event-card',
+  templateUrl: './event-card.component.html',
+  standalone: false
 })
 export class EventCardComponent {
-  @Input() src: string = "#";
+  @Input() src!: string;
+  @Input() href!: string;
+  @Input() name!: string;
+  @Input() venue!: string;
+  @Input() time!: string;
 }
