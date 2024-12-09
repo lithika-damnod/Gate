a98435d9943095879f95b1e2dd760cd1a387b5c7
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2024-12-09 10:40:34 +0530

    created password strength level showing component

--- frontend/src/app/shared/components/input/input.component.ts
+++ frontend/src/app/shared/components/input/input.component.ts
@@ -17,17 +17,29 @@ import { Component, Input } from '@angular/core';
       >
         <input [type]="passwordSecondaryType" 
             class="overflow-hidden w-full p-2 px-4 text-[0.85rem] bg-black placeholder:text-[#ffffffb3] border-none outline-none flex-shrink"
-            [placeholder]="placeholder" />
+            [placeholder]="placeholder"
+            (input)="onInput($event)"
+            />
         <button class="w-[28px] h-[24px] scale-75 mr-2" (click)="togglePasswordVisibility()">
           <app-eye [color]="color" /> 
         </button>
       </div>
       <div class="mt-3.5 flex w-full h-[3px] gap-2 px-1" *ngIf="strength">
-          <div class="w-full h-full bg-[#f0f2a3b3] flex-shrink"></div>
-          <div class="w-full h-full bg-[#f0f2a3b3] flex-shrink"></div>
-          <div class="w-full h-full bg-[#f0f2a3b3] flex-shrink"></div>
-          <div class="w-full h-full bg-[#f0f2a3b3] flex-shrink"></div>
-          <div class="w-full h-full bg-[#f0f2a3b3] flex-shrink"></div>
+          <div class="w-full h-full flex-shrink transition-colors"
+            [ngStyle]="{'background-color': setScoreColor(1)}"
+          ></div>
+          <div class="w-full h-full flex-shrink transition-colors"
+            [ngStyle]="{'background-color': setScoreColor(2)}"
+          ></div>
+          <div class="w-full h-full flex-shrink transition-colors"
+            [ngStyle]="{'background-color': setScoreColor(3)}"
+          ></div>
+          <div class="w-full h-full flex-shrink transition-colors"
+            [ngStyle]="{'background-color': setScoreColor(4)}"
+          ></div>
+          <div class="w-full h-full flex-shrink transition-colors"
+            [ngStyle]="{'background-color': setScoreColor(5)}"
+          ></div>
       </div>
     </ng-container>
   `,
@@ -42,5 +54,40 @@ export class InputComponent {
   togglePasswordVisibility() {
     this.color = this.color === "white" ? "#fc9090" : "white";
     this.passwordSecondaryType = this.passwordSecondaryType === "password" ? "text" : "password";
+  }
+
+
+  score: number = 0;
+  scoreColor: string = "#D9D9D9";
+  onInput(event: Event) {
+    this.score = 0;
+
+    const element = event.target as HTMLInputElement;
+    let password: string = element.value;
+    if (password.length >= 8) this.score++; // Length of 8 or more
+    if (/[a-z]/.test(password)) this.score++; // Contains lowercase
+    if (/[A-Z]/.test(password)) this.score++; // Contains uppercase
+    if (/\d/.test(password)) this.score++; // Contains number
+    if (/[\W_]/.test(password)) this.score++; // Contains special character
+
+    console.log("strength: ", this.score);
+  }
+
+  setScoreColor(index: number): string {
+    switch (this.score) {
+      case 0:
+      default:
+        return "#d9d9d9";
+      case 1:
+        return index <= 1 ? "#e39299" : "#D9D9D9";
+      case 2:
+        return index <= 2 ? "#f7aa83b3" : "#D9D9D9";
+      case 3:
+        return index <= 3 ? "#ffe093" : "#D9D9D9";
+      case 4:
+        return index <= 4 ? "#a9f08b" : "#D9D9D9";
+      case 5:
+        return index <= 5 ? "#78c17c" : "#D9D9D9";
+    }
   }
 }
