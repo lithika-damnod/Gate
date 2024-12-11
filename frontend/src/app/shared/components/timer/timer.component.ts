48583a385a41f988698b5b0215b18c460b348086
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2024-12-11 19:14:35 +0530

    connected time field to the timer to show the remaining time

--- frontend/src/app/shared/components/timer/timer.component.ts
+++ frontend/src/app/shared/components/timer/timer.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 
 @Component({
     selector: 'app-timer',
@@ -26,11 +27,63 @@ import { Component, Input } from '@angular/core';
         <div class="border-[0.7px] border-[#ffffff30] h-5"></div>
     </div>
     <div class="text-[0.8rem] text-gray-200" *ngIf="size === 'small'">
-        06 Hours | 24 Minutes | 31 Seconds
+        {{this.timeRemaining.hours}} Hours | {{this.timeRemaining.minutes}} Minutes | {{this.timeRemaining.seconds}} Seconds
     </div>
   `,
     standalone: false
 })
-export class TimerComponent {
+export class TimerComponent implements OnInit, OnDestroy {
     @Input() size: string = "large";
+    @Input() targetDate!: Date; // Target date
+
+    timeRemaining: { days: number, hours: number; minutes: number; seconds: number; } = {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0
+    }
+
+    private countdown$: Subscription | undefined;
+
+    ngOnInit(): void {
+        this.startCountDown();
+    }
+    startCountDown(): void {
+        this.countdown$ = interval(1000).subscribe(() => {
+            this.updateRemainingTime();
+            console.log("time remaining: ", this.timeRemaining);
+        });
+    }
+
+    updateRemainingTime(): void {
+        const now = new Date().getTime();
+        const target = this.targetDate.getTime();
+        const difference = target - now;
+
+        if (difference > 0) {
+            this.timeRemaining = this.calculateTime(difference);
+        } else {
+            this.timeRemaining = { days: 0, hours: 0, minutes: 0, seconds: 0 };
+            this.stopCountdown();
+        }
+    }
+
+    calculateTime(difference: number): { days: number; hours: number; minutes: number; seconds: number } {
+        const seconds = Math.floor((difference / 1000) % 60);
+        const minutes = Math.floor((difference / (1000 * 60)) % 60);
+        const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+
+        return { days, hours, minutes, seconds };
+    }
+
+    stopCountdown(): void {
+        if (this.countdown$) {
+            this.countdown$.unsubscribe();
+        }
+    }
+
+    ngOnDestroy(): void {
+        this.stopCountdown(); // clean subscription when the component is destroyed
+    }
 }
