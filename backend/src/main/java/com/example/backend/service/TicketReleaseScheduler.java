35ebc52be23c07013f7e86451b289ddae161963f
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2024-12-12 05:02:39 +0530

    defined proper ticket releasing logic

--- backend/src/main/java/com/example/backend/service/TicketReleaseScheduler.java
+++ backend/src/main/java/com/example/backend/service/TicketReleaseScheduler.java
@@ -1,6 +1,8 @@
 package com.example.backend.service;
 
+import com.example.backend.config.GlobalConfig;
 import com.example.backend.websocket.CustomWebSocketHandler;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,13 +15,23 @@ import java.util.Map;
 public class TicketReleaseScheduler {
     private final TicketTypeService ticketTypeService;
     private final CustomWebSocketHandler customWebSocketHandler;
+    private final GlobalConfig globalConfig;
 
-    @Scheduled(fixedRate =  10 * 1000)
+    public Integer rate;
+
+    @PostConstruct
+    public void init() {
+        rate = globalConfig.getUserRetrievalRate();
+    }
+
+    @Scheduled(fixedRate = 10000)
     public void scheduleTicketRelease() {
         ticketTypeService.releaseTickets();
+        /*
         Map<String, Object> returnMsg = new HashMap<>();
         returnMsg.put("release", "tickets");
 
         customWebSocketHandler.broadcast(returnMsg);
+         */
     }
 }
--- backend/src/main/java/com/example/backend/service/TicketTypeService.java
+++ backend/src/main/java/com/example/backend/service/TicketTypeService.java
@@ -2,31 +2,61 @@ package com.example.backend.service;
 
 import com.example.backend.models.TicketType;
 import com.example.backend.repository.TicketTypeRepository;
+import com.example.backend.websocket.CustomWebSocketHandler;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class TicketTypeService {
     private final TicketTypeRepository ticketTypeRepository;
+    private final CustomWebSocketHandler customWebSocketHandler;
 
     @Transactional
     public void releaseTickets() {
         List<TicketType> ticketTypes = ticketTypeRepository.findAll();
         for(TicketType ticketType : ticketTypes){
             int total = ticketType.getTotal();
-            int ticketsBooked = ticketType.getNumberOfTickets();
+            int pool = ticketType.getNumberOfTickets();
             int releaseRate = ticketType.getTicketReleaseRate();
+            int maxTicketCapacity =  ticketType.getMaxTicketCapacity();
 
-            if(ticketsBooked < total){
-                int ticketsToRelease = Math.min(releaseRate, total - ticketsBooked);
-                ticketType.setTicketReleaseRate(ticketsBooked + ticketsToRelease);
+            /*
+            if(pool < total){
+                int ticketsToRelease = Math.min(releaseRate, maxTicketCapacity - pool);
+                if((ticketsToRelease+pool) > total) {
+                    ticketsToRelease = total;
+                }
+                ticketType.setNumberOfTickets(pool + ticketsToRelease);
             }
-        }
+            */
 
-        ticketTypeRepository.saveAll(ticketTypes);
+            if(pool < maxTicketCapacity && total > 0) { // if total is zero there's nothing possible to be done
+                int nTicketsToRelease = Math.min(releaseRate, maxTicketCapacity - pool);
+                if(total >= nTicketsToRelease) { // there's enough tickets left to be added to the pool
+                    total-=nTicketsToRelease;
+                    pool+=nTicketsToRelease;
+                }
+                else {
+                    pool += total;
+                    total = 0;
+                }
+
+                ticketType.setNumberOfTickets(pool);
+                ticketType.setTotal(total);
+                TicketType saved = ticketTypeRepository.save(ticketType);
+
+                // releasing
+                Map<String, Object> returnMsg = new HashMap<>();
+                returnMsg.put("release", saved);
+                customWebSocketHandler.broadcast(returnMsg);
+            }
+
+        }
     }
 }
