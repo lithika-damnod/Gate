ae50455a17bd60a69ff37c2abe3a3c6745d9f96d
Lithika Damnod <l.d.b.dasanayaka@gmail.com>
2024-12-12 07:25:03 +0530

    used api calls to print out relavent event information

--- frontend/src/app/features/configuration/event-list/event-list.component.ts
+++ frontend/src/app/features/configuration/event-list/event-list.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-event-list',
   standalone: false,
-  
+
   templateUrl: './event-list.component.html',
   styleUrl: './event-list.component.css'
 })
 export class EventListComponent {
-
+  @Input() event!: any;
 }
--- frontend/src/app/pages/configuration/configuration.component.ts
+++ frontend/src/app/pages/configuration/configuration.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { EventsService } from './../../shared/services/events.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-configuration',
   standalone: false,
-  
+
   templateUrl: './configuration.component.html',
   styleUrl: './configuration.component.css'
 })
-export class ConfigurationComponent {
+export class ConfigurationComponent implements OnInit {
+  constructor(private eventService: EventsService) { }
 
+  events: any;
+  ngOnInit(): void {
+    this.events = this.eventService.getEvent().subscribe({
+      next: (response) => {
+        this.events = response;
+        console.log(this.events);
+      },
+      error: (error) => {
+        console.log("Error fetching events", error);
+      },
+    });
+  }
 }
